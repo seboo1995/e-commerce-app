bf362273f8cd3edc659da460c9278d81631fba71
Nadica <96199677+nadicaristic@users.noreply.github.com>
2022-09-19 20:30:24 +0200

    menu-popup css

--- src/components/MenuPop/MenuPop.js
+++ src/components/MenuPop/MenuPop.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './menu-pop.css'
 
 export const MenuPop = (props) => {
 
-    //this should be in database maybe
+  //this should be in database maybe
   const all_items = {
     "home-items": {
       columns: ["styles-home", "topic-home", "brands-home"],
@@ -16,8 +17,8 @@ export const MenuPop = (props) => {
       ],
       column_3: ["brandname", "slothing", "masore", "gap"],
     },
-    'shop-items':{
-        columns: ["styles-shop", "topic-shop", "brands-shop"],
+    'shop-items': {
+      columns: ["styles-shop", "topic-shop", "brands-shop"],
       column_1: ["default", "full-width", "modern", "collections", "Dark Skin"],
       column_2: [
         "furniture1",
@@ -29,8 +30,8 @@ export const MenuPop = (props) => {
       column_3: ["brandname", "slothing", "masore", "gap"],
 
     },
-    'product-items':{
-        columns: ["styles-product", "topic-product", "brands-product"],
+    'product-items': {
+      columns: ["styles-product", "topic-product", "brands-product"],
       column_1: ["default", "full-width", "modern", "collections", "Dark Skin"],
       column_2: [
         "furniture1",
@@ -46,17 +47,19 @@ export const MenuPop = (props) => {
   const target_obj = all_items[props.menu_item]
   console.log(target_obj)
   return (
-    <div>
-        {target_obj.columns.map(item => {
-            return (<div className='flex-container'> 
-                {item}
-                </div>)
-        })}
-        
 
+    <div className="menu-popup-container">
+      {target_obj.columns.map(item => {
+        return (<div className='popup-container'>
+          <div className="item-container">
 
+            <p> {item}</p>
+          </div>
 
+        </div>)
+      })}
 
     </div>
+
   );
 };
